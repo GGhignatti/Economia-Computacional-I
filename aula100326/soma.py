7d6e29595da278376001b273d9a9e1f86b85a89c
GGhignatti <brazflair@gmail.com>
2026-04-07 13:55:38 -0300

    Add files via upload

---
+++ aula100326/soma.py
@@ -0,0 +1,25 @@
+""" 
+soma
+Descrição: Este programa lê dois números do programador, calcula a soma deles e imprime na tela.
+Autor: Gabriel G. Ghignatti
+Data: 10/10/2026
+Versão: 0.0.1
+"""
+
+# Alocação de memória
+
+numero_1= 2
+numero_2= 3
+
+soma = 0
+
+# Entrada de dados
+
+
+
+# Processamento de dados
+
+soma = numero_1 + numero_2
+
+# Saída de Dados
+print(soma)
