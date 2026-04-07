49b29f6abaff9dabe29c19248ce1fb05a73e7bc1
GGhignatti <brazflair@gmail.com>
2026-04-07 13:54:58 -0300

    Create nome_do_programa.py

---
+++ aula100326/nome_do_programa.py
@@ -0,0 +1,22 @@
+""" 
+nome_do_programa
+
+Descrição: Este programa explica a estrutura básica de um código escrito na linguagem de programação Python
+Autor: Gabriel G. Ghignatti
+Data: 10/10/2026
+Versão: 1.0.0
+"""
+
+# Alocação de memória
+
+
+
+# Entrada de dados
+
+
+
+# Processamento de dados
+
+
+
+# Saída de Dados
