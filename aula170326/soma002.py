95430573d96b39f2b086b174342941eca838815c
GGhignatti <brazflair@gmail.com>
2026-04-07 13:59:27 -0300

    Add files via upload

---
+++ aula170326/soma002.py
@@ -0,0 +1,24 @@
+# Programa soma002
+# Descrição: Este programa lê dois números inteiros digitados pelo usuário e calcula a soma deles.
+# Autor: Gabriel G. Ghignatti
+# Versão: 0.0.3
+
+# Alocação de Memória
+
+numero_1 = 0
+
+numero_2 = 0
+
+soma = 0
+
+# Entrada de Dados
+numero_1 = input("Qual a primeira parcela: ")
+
+numero_2 = input("Qual o segunda parcela: ")
+
+# Processamento de Dados: Cálculo da Soma
+
+soma = numero_1 + numero_2
+
+# Saída de Dados
+print(soma)
---
+++ aula170326/soma003.py
@@ -0,0 +1,26 @@
+# Programa soma002
+# Descrição: Este programa lê dois números inteiros digitados pelo usuário e calcula a soma deles.
+# Autor: Gabriel G. Ghignatti
+# Data: 17/03/2026
+# Versão: 0.0.3
+# Notas da versão: Correção do erro semântico no uso da função input, a qual produzia a concatenação de textos ao em vez de soma de números.
+
+# Alocação de Memória
+
+numero_1 = 0
+
+numero_2 = 0
+
+soma = 0
+
+# Entrada de Dados
+numero_1 = int(input("Qual a primeira parcela: ")) # Acréscimo da função int()
+
+numero_2 = int(input("Qual o segunda parcela: ")) # Acréscimo da função int()
+
+# Processamento de Dados: Cálculo da Soma
+
+soma = numero_1 + numero_2
+
+# Saída de Dados
+print(soma)
